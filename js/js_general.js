19a627e192dc7dba5f17a7b35e34bc4c1481afda
Rakultur <76803753+Rakultur@users.noreply.github.com>
2021-06-28 13:41:03 -0500

    Todos los archivos

---
+++ js/js_general.js
@@ -0,0 +1,7 @@
+// Menu responsive
+const burgerIcon = document.querySelector('#burger');
+const navbarMenu = document.querySelector('#nav-links');
+
+burgerIcon.addEventListener('click', () =>{
+    navbarMenu.classList.toggle('is-active')
+})
